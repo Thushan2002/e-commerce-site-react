261f650c4d490d79a65855ceae1d0f91bd054426
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-03-11 20:56:46 +0530

    feat: footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
           <Route path="/" element={<Navbar />} />
         </Routes>
       </Router>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container footer_container">
+        <div className="footer_item">
+          <a href="#" className="footer_logo">
+            Exclusive
+          </a>
+          <div className="footer_p">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Exercitationem fuga harum voluptate?
+          </div>
+        </div>
+        <div className="footer_item">
+          <h3 className="footer_item_titl">Support</h3>
+          <ul className="footer_list">
+            <li className="li footer_list_item">Stockholm, Sweden</li>
+            <li className="li footer_list_item">email@gmail.com</li>
+            <li className="li footer_list_item">+46 123 456 78</li>
+            <li className="li footer_list_item">+46 72 345 67</li>
+          </ul>
+        </div>
+        <div className="footer_item">
+          <h3 className="footer_item_titl">Support</h3>
+          <ul className="footer_list">
+            <li className="li footer_list_item">Account</li>
+            <li className="li footer_list_item">Login / Register</li>
+            <li className="li footer_list_item">Cart</li>
+            <li className="li footer_list_item">Shop</li>
+          </ul>
+        </div>
+        <div className="footer_item">
+          <h3 className="footer_item_titl">Support</h3>
+          <ul className="footer_list">
+            <li className="li footer_list_item">Privacy policy</li>
+            <li className="li footer_list_item">Terms of use</li>
+            <li className="li footer_list_item">FAQ's</li>
+            <li className="li footer_list_item">Contact</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer_bottom">
+        <div className="container footer_bottom_container">
+          <p className="footer_copy">
+            Copyright Exclusive 2023. All right reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
